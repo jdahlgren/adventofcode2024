203a9c2b775130429f31ff79984618f0c6a5b17c
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2024-11-27 21:43:16 +0100

    first commit

---
+++ src/main/java/se/johannesdahlgren/aoc24/Day1.java
@@ -0,0 +1,10 @@
+package se.johannesdahlgren.aoc24;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Day1 {
+
+  private static final Logger log = LoggerFactory.getLogger(Day1.class);
+
+}
