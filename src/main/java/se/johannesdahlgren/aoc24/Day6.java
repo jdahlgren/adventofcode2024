25d866931518e8eecceea42a1eb62e42c258b5e8
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2024-12-07 23:33:30 +0100

    Day 6: Lets reuse methods between part 1 and two

--- src/main/java/se/johannesdahlgren/aoc24/Day6.java
+++ src/main/java/se/johannesdahlgren/aoc24/Day6.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Set;
 
 public class Day6 {
-
   private enum Direction {
     NORTH(0, -1),
     EAST(1, 0),
@@ -33,28 +32,62 @@ public class Day6 {
     }
   }
 
-  private record Position(int x, int y) {
+  private record Position(int x, int y) {}
+  private record State(Position position, Direction direction) {}
 
-  }
-
-  public int countDistinctLocations() throws IOException {
+  public int part1() throws IOException {
     List<String> lines = Files.readAllLines(Path.of("src/main/resources/day6"));
     char[][] map = createMap(lines);
     Position startPos = findStartPosition(map);
-    Set<Position> visitedPositions = new HashSet<>();
+    return simulateGuard(map, startPos).visitedPositions().size();
+  }
 
+  public int part2() throws IOException {
+    List<String> lines = Files.readAllLines(Path.of("src/main/resources/day6"));
+    char[][] originalMap = createMap(lines);
+    Position startPos = findStartPosition(originalMap);
+    int loopCount = 0;
+
+    for (int y = 0; y < originalMap.length; y++) {
+      for (int x = 0; x < originalMap[y].length; x++) {
+        if (originalMap[y][x] == '#' || (x == startPos.x && y == startPos.y)) {
+          continue;
+        }
+
+        char[][] mapCopy = copyMap(originalMap);
+        mapCopy[y][x] = '#';
+
+        if (simulateGuard(mapCopy, startPos).isLoop()) {
+          loopCount++;
+        }
+      }
+    }
+
+    return loopCount;
+  }
+
+  private record SimulationResult(Set<Position> visitedPositions, boolean isLoop) {}
+
+  private SimulationResult simulateGuard(char[][] map, Position startPos) {
+    Set<Position> visitedPositions = new HashSet<>();
+    Set<State> visitedStates = new HashSet<>();
     Direction currentDirection = Direction.NORTH;
     Position currentPos = startPos;
     visitedPositions.add(currentPos);
 
     while (true) {
+      State currentState = new State(currentPos, currentDirection);
+      if (!visitedStates.add(currentState)) {
+        return new SimulationResult(visitedPositions, true);
+      }
+
       Position nextPos = new Position(
           currentPos.x + currentDirection.dx,
           currentPos.y + currentDirection.dy
       );
 
       if (!isInsideMap(nextPos, map)) {
-        break;
+        return new SimulationResult(visitedPositions, false);
       }
 
       if (isObstacle(nextPos, map)) {
@@ -64,9 +97,11 @@ public class Day6 {
 
       currentPos = nextPos;
       visitedPositions.add(currentPos);
-    }
 
-    return visitedPositions.size();
+      if (visitedStates.size() > map.length * map[0].length * 4) {
+        return new SimulationResult(visitedPositions, false);
+      }
+    }
   }
 
   private char[][] createMap(List<String> lines) {
@@ -88,6 +123,14 @@ public class Day6 {
     throw new IllegalStateException("No start position found");
   }
 
+  private char[][] copyMap(char[][] original) {
+    char[][] copy = new char[original.length][];
+    for (int i = 0; i < original.length; i++) {
+      copy[i] = original[i].clone();
+    }
+    return copy;
+  }
+
   private boolean isInsideMap(Position pos, char[][] map) {
     return pos.y >= 0 && pos.y < map.length &&
         pos.x >= 0 && pos.x < map[pos.y].length;
@@ -97,84 +140,11 @@ public class Day6 {
     return map[pos.y][pos.x] == '#';
   }
 
-  public int part2() throws IOException {
-    List<String> lines = Files.readAllLines(Path.of("src/main/resources/day6"));
-    char[][] originalMap = createMap(lines);
-    Position startPos = findStartPosition(originalMap);
-    int loopCount = 0;
-
-    for (int y = 0; y < originalMap.length; y++) {
-      for (int x = 0; x < originalMap[y].length; x++) {
-        // Skip positions that are already obstacles or the start position
-        if (originalMap[y][x] == '#' || (x == startPos.x && y == startPos.y)) {
-          continue;
-        }
-
-        char[][] mapCopy = copyMap(originalMap);
-        mapCopy[y][x] = '#';  // Place new obstacle
-
-        if (createsLoop(mapCopy, startPos)) {
-          loopCount++;
-        }
-      }
-    }
-
-    return loopCount;
-  }
-
-  private boolean createsLoop(char[][] map, Position startPos) {
-    Set<State> visitedStates = new HashSet<>();
-    Direction currentDirection = Direction.NORTH;
-    Position currentPos = startPos;
-
-    while (true) {
-      State currentState = new State(currentPos, currentDirection);
-
-      // If we've seen this state before, we're in a loop
-      if (!visitedStates.add(currentState)) {
-        return true;
-      }
-
-      Position nextPos = new Position(
-          currentPos.x + currentDirection.dx,
-          currentPos.y + currentDirection.dy
-      );
-
-      // If we exit the map, it's not a loop
-      if (!isInsideMap(nextPos, map)) {
-        return false;
-      }
-
-      if (isObstacle(nextPos, map)) {
-        currentDirection = currentDirection.turnRight();
-        continue;
-      }
-
-      currentPos = nextPos;
-
-      // If we've visited too many states, assume it's not a loop
-      if (visitedStates.size() > map.length * map[0].length * 4) {
-        return false;
-      }
-    }
-  }
-
-  private char[][] copyMap(char[][] original) {
-    char[][] copy = new char[original.length][];
-    for (int i = 0; i < original.length; i++) {
-      copy[i] = original[i].clone();
-    }
-    return copy;
-  }
-
-  private record State(Position position, Direction direction) {}
-
-
-  public static void main(String[] args) throws IOException {
+public static void main(String[] args) throws IOException {
     Day6 day6 = new Day6();
-    int distinctLocations = day6.countDistinctLocations();
+    int part1 = day6.part1();
     int part2 = day6.part2();
-    System.out.println("Number of distinct locations visited: " + distinctLocations);
+    System.out.println("Part 1: " + part1);
     System.out.println("Part 2: " + part2);
 
   }
